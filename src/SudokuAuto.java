baffbff64eee7d92d885f67575a47ae347301346
Sylwester Leyk <sylwester95leyk@gmail.com>
2023-02-21 22:01:11 +0100

    modyfikacja pliku SudokuAuto

--- src/SudokuAuto.java
+++ src/SudokuAuto.java
@@ -3,7 +3,7 @@ public class SudokuAuto {
 
         // wartości sudoku
         int x = 0;
-        int[][] sudoku = {{1, 4, 3, 2}, {4, 1, 2, 3}, {x, 3, 4, 1}, {3, x, 1, 4}};
+        int[][] sudoku = {{1, x, 3, 2}, {4, x, 2, 3}, {x, 3, 4, 1}, {3, x, 1, 4}};
 
         // wyświetlenie sudoku
         System.out.println("Wprowadzone sudoku:");
@@ -18,12 +18,10 @@ public class SudokuAuto {
             System.out.println();
         }
 
-        //wyznaczenie ile wynosi 'sum' na podstawie długości tablicy
-        int sum = sudoku.length;
-        int z = 1;
-        while ((sudoku.length - z) > 0) {
-            sum += sudoku.length - z;
-            z++;
+        // obliczanie ile wynosi sum
+        int sum = 0, z = 0;
+        while (z < sudoku.length) {
+            sum += ++z;
         }
 
         // obliczenie brakującej cyfry oraz zapamiętanie jej indeksu
@@ -33,33 +31,30 @@ public class SudokuAuto {
             int emptyRowIndex = 0;
             int emptyColIndex = 0;
             int niewiadoma = sum;
-                while (colIndex < sudoku[rowIndex].length) {
-                    niewiadoma -= sudoku[rowIndex][colIndex];
-                    if (sudoku[rowIndex][colIndex] == 0) {
-                        emptyRowIndex = rowIndex;
-                        emptyColIndex = colIndex;
-                    }
-                    colIndex++;
+            while (colIndex < sudoku[rowIndex].length) {
+                niewiadoma -= sudoku[rowIndex][colIndex];
+                if (sudoku[rowIndex][colIndex] == 0) {
+                    emptyRowIndex = rowIndex;
+                    emptyColIndex = colIndex;
                 }
-                sudoku[emptyRowIndex][emptyColIndex] = niewiadoma;
-                rowIndex++;
+                colIndex++;
             }
+            sudoku[emptyRowIndex][emptyColIndex] = niewiadoma;
+            rowIndex++;
+        }
 
-            // wyświetlenie sudoku z podstawionymi niewiadomymi
+        // wyświetlenie sudoku z podstawionymi niewiadomymi
 
-            System.out.println("Uzupełnione sudoku:");
-            rowIndex = 0;
-            while (rowIndex < sudoku.length) {
-                int colIndex = 0;
-                while (colIndex < sudoku[rowIndex].length) {
-                    System.out.print(sudoku[rowIndex][colIndex] + " ");
-                    colIndex++;
-                }
-                rowIndex++;
-                System.out.println();
+        System.out.println("Uzupełnione sudoku:");
+        rowIndex = 0;
+        while (rowIndex < sudoku.length) {
+            int colIndex = 0;
+            while (colIndex < sudoku[rowIndex].length) {
+                System.out.print(sudoku[rowIndex][colIndex] + " ");
+                colIndex++;
             }
+            rowIndex++;
+            System.out.println();
         }
     }
-
-
-
+}
